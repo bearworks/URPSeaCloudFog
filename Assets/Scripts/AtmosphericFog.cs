fe4310d0f8233a85dc579a7a07b8a5b791052c51
bearworks <196221347@qq.com>
2022-08-28 21:18:34 +0800

    Initial Commit

---
+++ Assets/Scripts/AtmosphericFog.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+[ExecuteAlways]
+public class AtmosphericFog : MonoBehaviour
+{
+    [HeaderAttribute("Height Fog")]
+    public float skyFogDistanceFactor = 0.5f;
+    [Range(0.1f,1.2f)]
+    public float skyFogRange = 0.5f;
+    public float skyFogStart = 0f;
+
+    [Space]
+    public float heightFogDistanceFactor = 0.5f;
+    public float heightFogRange = 0.5f;
+    public float heightFogStart = 0f;
+
+    [Space]
+    [Range(0.01f, 0.98f)]
+    public float mMieAsymmetry = 0f;
+
+    [ColorUsage(false, true)]
+    public Color mAlbedoR;
+
+    [ColorUsage(false, true)]
+    public Color mAlbedoM;
+
+    [ColorUsage(false, true)]
+    public Color mSunColor;
+
+    [ColorUsage(false, true)]
+    public Color mAmbColor;
+
+    [HeaderAttribute("Scattering Weights")]
+    public Vector4 betaRsMs = Vector4.zero;
+    public float betaMa = 0f;
+
+    // Update is called once per frame
+    void Update()
+    { 
+        RenderSettings.fogMode = FogMode.Linear;
+
+        Shader.SetGlobalVector("_SkyFogParams", new Vector4(Mathf.Max(0.0f, skyFogDistanceFactor), skyFogRange, skyFogStart, 0f));
+        Shader.SetGlobalVector("_HeightFogParams", new Vector4(heightFogDistanceFactor, heightFogRange, heightFogStart, 0f));
+
+        Shader.SetGlobalVector("_BetaRsMs", betaRsMs);
+        Shader.SetGlobalFloat("_BetaMa", betaMa);
+        Shader.SetGlobalFloat("mMieAsymmetry", mMieAsymmetry);
+
+        Shader.SetGlobalColor("mAlbedoR", mAlbedoR);
+        Shader.SetGlobalColor("mAlbedoM", mAlbedoM);
+        Shader.SetGlobalColor("mSunColor", mSunColor);
+        Shader.SetGlobalColor("mAmbColor", mAmbColor);
+	}
+
+    private void Reset()
+    {
+        Shader.SetGlobalVector("_SkyFogParams", new Vector4(0, 1, 1, 0));
+        Shader.SetGlobalVector("_HeightFogParams", new Vector4(0, 1, 1, 0));
+        Shader.SetGlobalVector("_BetaRsMs", Vector4.one);
+        Shader.SetGlobalFloat("_BetaMa", 1);
+        Shader.SetGlobalFloat("mMieAsymmetry", 0.5f);
+
+        Shader.SetGlobalColor("mAlbedoR", Color.black);
+        Shader.SetGlobalColor("mAlbedoM", Color.black);
+        Shader.SetGlobalColor("mSunColor", Color.black);
+        Shader.SetGlobalColor("mAmbColor", Color.black);
+    }
+
+    private void OnDisable()
+    {
+        Reset();
+    }
+
+    private void Start()
+    {
+        Reset();
+    }
+
+}
---
+++ Assets/Scripts/ZenithGradientColor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ZenithGradientColor : MonoBehaviour
+{
+	public XClouds.XClouds cloud = null;
+	public AtmosphericFog fog = null;
+
+	[GradientUsageAttribute(true, ColorSpace.Gamma)]
+	public Gradient SunColor = new Gradient();
+	[GradientUsageAttribute(true, ColorSpace.Gamma)]
+	public Gradient AmbientColor = new Gradient();
+	[GradientUsageAttribute(true, ColorSpace.Gamma)]
+	public Gradient CloudSunColor = new Gradient();
+	[GradientUsageAttribute(true, ColorSpace.Gamma)]
+	public Gradient CloudBaseColor = new Gradient();
+
+	// Update is called once per frame
+	void Update()
+    {
+		if(cloud != null && fog != null)
+        {
+			float ForwardY = 0.75f;
+			if (RenderSettings.sun != null)
+			{
+				Vector3 Forward = (RenderSettings.sun.transform.rotation * Vector3.forward).normalized;
+				ForwardY = -Forward.y;
+				ForwardY = Mathf.Max(ForwardY * 0.5f + 0.5f, 0f);
+			}
+
+			cloud.cloudBaseColor = CloudBaseColor.Evaluate(ForwardY);
+			cloud.cloudSunColor = CloudSunColor.Evaluate(ForwardY);
+
+			fog.mSunColor = SunColor.Evaluate(ForwardY);
+			fog.mAmbColor = AmbientColor.Evaluate(ForwardY);
+			cloud.cloudTopColor = fog.mAmbColor;
+		}
+	}
+
+}
